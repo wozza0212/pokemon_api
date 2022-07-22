b493631f05d418462bab837870a4b676e6e30dc0
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-07-22 23:04:33 +0100

    Added Sprite url

--- pokemon_api_reader.py
+++ pokemon_api_reader.py
@@ -13,7 +13,7 @@ for x in range(1,151):
     pokemon = requests.get(new_url)
     pokemon = pokemon.json()
     pokemon_info.append({'name': pokemon['name'], 'id':pokemon['id'], 'height':pokemon['height'],
-    'weight': pokemon['weight'], 'type':(pokemon['types'][0]['type']['name'])})
+    'weight': pokemon['weight'], 'type':(pokemon['types'][0]['type']['name']), 'sprite url':pokemon['sprites']['other']['official-artwork']['front_default']})
 
 
 with open('pokemon_info.csv', 'w', encoding='utf8', newline='') as output_file:
@@ -22,4 +22,13 @@ with open('pokemon_info.csv', 'w', encoding='utf8', newline='') as output_file:
     dict_writer.writeheader()
     dict_writer.writerows(pokemon_info)
 
-    
+# url = ('https://pokeapi.co/api/v2/pokemon/')
+# response = requests.get(url)
+# pokemon_info = []
+# all_data = response.json()
+# for x in range(1,2):
+#     new_url = f'{url}{x}'
+#     pokemon = requests.get(new_url)
+#     pokemon = pokemon.json()
+#     sprite = pokemon['sprites']['other']['official-artwork']['front_default']
+#     print(sprite)
