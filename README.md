6bfa00e85eb5741a85c2526023da8d774ef0dc80
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-07-27 18:29:46 +0100

    altered to get images as well

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pokemon_api
+
+This is a simple project using the public pokemon api, The program acts with the api to 
+geto the number, name, height, weight, type, and image of each of the original 150 pokemon,
+my plan for this data is to make a chart comparing the heights of the original pokemon
--- pokemon_api_reader.py
+++ pokemon_api_reader.py
@@ -14,14 +14,25 @@ for x in range(1,151):
     pokemon = pokemon.json()
     pokemon_info.append({'name': pokemon['name'], 'id':pokemon['id'], 'height (decimeters)':pokemon['height'],
     'weight': pokemon['weight'], 'type':(pokemon['types'][0]['type']['name']), 'sprite url':pokemon['sprites']['other']['official-artwork']['front_default']})
+    sprite_url = pokemon['sprites']['other']['official-artwork']['front_default']
+    image_name = pokemon['name']
+    img_data = requests.get(sprite_url).content
+    with open(f'images/{image_name}.png', 'wb') as handler:
+        handler.write(img_data)
 
 
+
+
+
+#Commented out while trying to save images
 with open('pokemon_info.csv', 'w', encoding='utf8', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, fieldnames=pokemon_info[0].keys())
 
     dict_writer.writeheader()
     dict_writer.writerows(pokemon_info)
 
+
+# Originsl code, commented out, dont currently need
 # url = ('https://pokeapi.co/api/v2/pokemon/')
 # response = requests.get(url)
 # pokemon_info = []
